009cd111669f6bedeb90f534f12f4bacd2574277
Luke Thompson <endeavour9@gmail.com>
2022-04-01 17:44:12 +1100

    timing with the mongo driver (bundle size now 5.8MB)

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Initial version (basic deps - Jackson, Log4J, AWS handler interface):
 After adding the environment variable:  
 JAVA_TOOL_OPTIONS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1"  
 ![img_1.png](img_1.png)
+
+Add the Mongo driver and print the version in the response:
+![img_2.png](img_2.png)
--- src/main/java/com/mycodefu/Handler.java
+++ src/main/java/com/mycodefu/Handler.java
@@ -3,6 +3,7 @@ package com.mycodefu;
 import java.util.Collections;
 import java.util.Map;
 
+import com.mongodb.internal.build.MongoDriverVersion;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,7 +17,7 @@ public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayRe
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: {}", input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+		Response responseBody = new Response(String.format("Go Serverless v1.x! Your function executed successfully! Mongo driver version %s.", MongoDriverVersion.VERSION), input);
 		return ApiGatewayResponse.builder()
 				.setStatusCode(200)
 				.setObjectBody(responseBody)
