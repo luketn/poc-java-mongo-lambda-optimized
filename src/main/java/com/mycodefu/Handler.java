b57f52969fa2150f885ed4e87ee4712119f05a8e
Luke Thompson <endeavour9@gmail.com>
2022-04-01 23:24:14 +1100

    try using data api

--- src/main/java/com/mycodefu/Handler.java
+++ src/main/java/com/mycodefu/Handler.java
@@ -4,61 +4,57 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.ServerApi;
-import com.mongodb.ServerApiVersion;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-import org.bson.json.JsonWriterSettings;
+import com.mongodb.client.model.Projections;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Handler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
-	private static String CONNECTION_STRING;
-	private static ConnectionString connectionString;
-	private static MongoClientSettings settings;
-	private static MongoClient mongoClient;
-	private static JsonWriterSettings jsonWriterSettings;
-
-
-	static {
-		long start = System.nanoTime();
-		CONNECTION_STRING = System.getenv("CONNECTION_STRING");
-		connectionString = new ConnectionString(CONNECTION_STRING);
-		settings = MongoClientSettings.builder()
-				.applyConnectionString(connectionString)
-				.serverApi(ServerApi.builder()
-						.version(ServerApiVersion.V1)
-						.build())
-				.build();
-		mongoClient = MongoClients.create(settings);
-		jsonWriterSettings = JsonWriterSettings.builder()
-				.objectIdConverter((objectId, strictJsonWriter) -> strictJsonWriter.writeString(objectId.toHexString()))
-				.build();
-		long end = System.nanoTime();
-		long timeTakenMillis = (end - start) / 1_000_000;
-		System.out.println(String.format("Took %d milliseconds to initialize MongoDB outside handler.", timeTakenMillis));
-	}
-
+	private static final String API_KEY = System.getenv("API_KEY");
+	private static final HttpClient httpClient = HttpClient.newHttpClient();
 
 	@Override
 	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent apiGatewayV2HTTPEvent, Context context) {
 //		System.out.printf("received: %s%n", apiGatewayV2HTTPEvent);
-
 		long start = System.nanoTime();
-		MongoDatabase database = mongoClient.getDatabase("ctest");
-		MongoCollection<Document> things = database.getCollection("things");
-		Document firstThing = things.find().first();
+
+		String responseBody;
+		try {
+			BsonDocument post = new BsonDocument();
+			post.append("dataSource", new BsonString("luke"));
+			post.append("database", new BsonString("ctest"));
+			post.append("collection", new BsonString("things"));
+			post.append("projection", Projections.include("_id").toBsonDocument());
+			String postJson = post.toJson();
+
+			HttpRequest request = HttpRequest
+					.newBuilder(new URI("https://data.mongodb-api.com/app/data-qqgpb/endpoint/data/beta/action/findOne"))
+					.POST(HttpRequest.BodyPublishers.ofString(postJson))
+					.header("Content-Type", "application/json")
+					.header("Access-Control-Request-Headers", "*")
+					.header("api-key", API_KEY)
+					.build();
+
+
+			HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+			responseBody = response.body();
+
+		} catch (InterruptedException | URISyntaxException | IOException e) {
+			throw new RuntimeException(e);
+		}
+
 		long end = System.nanoTime();
 		long timeTakenMillis = (end - start) / 1_000_000;
-		System.out.println(String.format("Took %d milliseconds to run MongoDB query inside handler.", timeTakenMillis));
+		System.out.printf("Took %d milliseconds to run MongoDB query inside handler.%n", timeTakenMillis);
 
-		String responseBody = firstThing == null ? "{}" : firstThing.toJson(jsonWriterSettings);
 
 		Map<String, String> headers = new HashMap<>();
 		headers.put("X-Powered-By", "AWS Lambda & serverless");
